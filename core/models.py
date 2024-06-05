cab18680d71ebaa8d8e500962caabfd68f3b2049
willianjonhy05 <willianjohny05@gmail.com>
2024-06-05 16:58:59 -0300

    Novas classes e mudanças

--- core/models.py
+++ core/models.py
@@ -29,7 +29,7 @@ class Areas(models.Model):
             self.codigo = self.gerar_codigo()
         super().save(*args, **kwargs)
 
-class Candidato(models.Model):
+class Usuario(models.Model):
     SEXO = (
         ('M', 'Masculino'),
         ('F', 'Feminino'),
@@ -59,6 +59,7 @@ class Candidato(models.Model):
     ]
         
     usuario = models.OneToOneField(User, verbose_name='Usuario', on_delete=models.CASCADE, blank=True, null=True, related_name='funcionario')
+    avatar = models.ImageField("Foto", upload_to='avatar/%Y/%m/%d/', blank=True, null=True)
     telefone = models.CharField("Telefone", max_length=11)
     nome = models.CharField("Nome Completo", max_length=100)
     sexo = models.CharField("Sexo", max_length=1, choices=SEXO)
@@ -96,8 +97,8 @@ class Candidato(models.Model):
         super().save(*args, **kwargs)
         
     class Meta:
-        verbose_name = "Candidato"
-        verbose_name_plural = "Candidatos"  
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"  
         
         
 class Empresa(models.Model):
@@ -111,7 +112,7 @@ class Empresa(models.Model):
         ('Baixada', 'Baixada'),
     )    
 
-    usuario = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.CASCADE, blank=True, null=True)
+    administrador =  models.ForeignKey(Usuario, verbose_name='Administrador da Empresa', on_delete=models.CASCADE)
     razao_social = models.CharField("Nome da Razão Social", max_length=50, blank=True, null=True)
     nome_fantasia = models.CharField("Nome Fantasia", max_length=50, blank=True, null=True)
     natureza_juridica = models.CharField("Natureza Jurídica", max_length=70, blank=True, null=True)
@@ -145,7 +146,78 @@ class Empresa(models.Model):
     def save(self, *args, **kwargs):
         self.validate_cnpj()
         super().save(*args, **kwargs)
+        
+    class Meta:
+        verbose_name = "Empresa"
+        verbose_name_plural = "Empresas"
 
 
-class Investidor(models.Model):
-    ...
+
+class Ideia(models.Model):
+    STATUS_CHOICES = [
+        ('Pendente', 'Pendente'),
+        ('Em Progresso', 'Em Progresso'),
+        ('Concluída', 'Concluída'),
+        ('Rejeitada', 'Rejeitada')
+    ]
+
+    TIPO_CHOICES = [
+        ('Produto', 'Produto'),
+        ('Serviço', 'Serviço')
+    ]
+    
+    titulo = models.CharField("Título da Ideia", max_length=100)
+    area = models.ForeignKey(Areas, on_delete=models.CASCADE, related_name='area')
+    descricao = models.TextField("Descrição da Ideia")
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='ideia')
+    data_criacao = models.DateTimeField("Data de Criação", auto_now_add=True)
+    data_atualizacao = models.DateTimeField("Data de Atualização", auto_now=True)
+    status = models.CharField("Status da Ideia", max_length=20, choices=STATUS_CHOICES, default='Pendente')
+    justificativa = models.TextField("Justificativa", blank=True, null=True)
+    objetivos = models.TextField("Objetivos", blank=True, null=True)
+    metodologia = models.TextField("Metodologia", blank=True, null=True)
+    cronograma = models.TextField("Cronograma", blank=True, null=True)
+    tipo = models.CharField("Tipo", max_length=20, choices=TIPO_CHOICES, default='Produto')
+    problematica = models.TextField("Problemática", blank=True, null=True)
+    publico_alvo = models.CharField("Público Alvo", max_length=255, blank=True, null=True)
+    viabilidade = models.TextField("Análise de Viabilidade", blank=True, null=True)
+    impacto = models.TextField("Impacto Esperado", blank=True, null=True)
+    investimento_desejado = models.DecimalField("Investimento Desejado (R$)", max_digits=10, decimal_places=2, blank=True, null=True)
+    resultados_esperados = models.TextField("Resultados Esperados", blank=True, null=True)
+    riscos = models.TextField("Riscos e Mitigações", blank=True, null=True)
+    parceiros = models.TextField("Parcerias Estratégicas", blank=True, null=True)
+    recursos_necessarios = models.TextField("Recursos Necessários", blank=True, null=True)
+    documentos = models.FileField("Outros documentos", upload_to='documentos/%Y/%m/%d/', null=True, blank=True)
+    
+    class Meta:
+        verbose_name = "Ideia"
+        verbose_name_plural = "Ideias"
+
+
+    def __str__(self):
+        return self.titulo
+    
+class Proposta(models.Model):
+    STATUS_CHOICES = [
+        ('Pendente', 'Pendente'),
+        ('Aceita', 'Aceita'),
+        ('Rejeitada', 'Rejeitada')
+    ]
+
+    ideia = models.ForeignKey(Ideia, on_delete=models.CASCADE, verbose_name='Ideia')
+    investidor = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name='Investidor')
+    data_proposta = models.DateTimeField("Data da Proposta", auto_now_add=True)
+    valor_investimento = models.DecimalField("Valor do Investimento (R$)", max_digits=10, decimal_places=2)
+    condicoes = models.TextField("Condições da Proposta")
+    status = models.CharField("Status da Proposta", max_length=20, choices=STATUS_CHOICES, default='Pendente')
+    retorno_esperado = models.DecimalField("Retorno Esperado (%)", max_digits=5, decimal_places=2, blank=True, null=True)
+    prazo_retorno = models.CharField("Prazo para Retorno do Investimento", max_length=100, blank=True, null=True)
+    comentarios = models.TextField("Comentários Adicionais", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Proposta"
+        verbose_name_plural = "Propostas"
+
+
+    def __str__(self):
+        return f"Proposta de {self.investidor.username} para {self.ideia.titulo}"
